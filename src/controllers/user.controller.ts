23dbd8b4142ee053eb5ead93c10321d671ed66f8
Marcos Andre <marcosdev.me@gmail.com>
2021-12-12 20:11:52 -0300

    creating functions find and create user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,9 +7,33 @@ export const getAll = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
-    
+    if(req.body.email && req.body.password) {
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+
+        const user = await User.findOne({email, password});
+
+        if(user) {
+            console.log("user", user);
+            res.json({ status: true });
+            return;
+        }
+    }
+
+    res.json({ status: false });
 }
 
-export const register = (req: Request, res: Response) => {
-    res.json({ message: 'endpoint register' });
+export const register = async (req: Request, res: Response) => {
+    if(req.body.name && req.body.email && req.body.password) {
+        const { name, email, password } = req.body;
+
+        const hasUser = await User.findByEmail(email);
+        if(!hasUser) {
+            const newUser = await User.create({name, email, password});
+
+            res.status(201).json({ status: true, user: newUser });
+        }
+    }
+
+    res.json({ error: 'Dados não enviados.' });
 }
--- src/services/User.ts
+++ src/services/User.ts
@@ -2,10 +2,42 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+type LoginProps = {
+    email: string;
+    password: string;
+}
+
+type RegisterProps = {
+    name: string;
+    email: string;
+    password: string;
+}
+
 export const User = {
     findAll: async () => {
         return await prisma.user.findMany({
             orderBy: { id: 'desc' }
+        });
+    },
+    findOne: async (data: LoginProps) => {
+        return await prisma.user.findFirst({
+            where: {
+                email: data.email, password: data.password
+            }
+        });
+    },
+    findByEmail: async (email: string) => {
+        return await prisma.user.findUnique({
+            where: { email }
+        });
+    },
+    create: async (data: RegisterProps) => {
+        return await prisma.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                password: data.password
+            }
         });
     }
 }
