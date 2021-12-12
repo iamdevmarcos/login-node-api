784779e153be7538b9f7555299ab6595a12fbd83
Marcos Andre <marcosdev.me@gmail.com>
2021-12-12 13:15:15 -0300

    adding email validator

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,35 +1,41 @@
 import { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
+import * as EmailValidator from 'email-validator'; 
 
 export const mail = async (req: Request, res: Response) => {
     const { from, subject, email } = req.body;
 
     if(from && subject && email) {
-        const transport = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'themindsetispowerfull@gmail.com',
-                pass: 'mindset6813fc!@$'
+        const emailChecked = EmailValidator.validate(from);
+        if(emailChecked) {
+            const transport = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'themindsetispowerfull@gmail.com',
+                    pass: 'mindset6813fc!@$'
+                }
+            });
+        
+            const message = {
+                from: 'nao-responda@imarcos.com',
+                to: 'Marcos Andre <marcosdev.me@gmail.com>',
+                replyTo: `${emailChecked}`,
+                subject: req.body.subject,
+                html: `<strong>${req.body.email}</strong>`,
+                text: req.body.email
             }
-        });
-    
-        const message = {
-            from: 'nao-responda@imarcos.com',
-            to: 'Marcos Andre <marcosdev.me@gmail.com>',
-            replyTo: req.body.from,
-            subject: req.body.subject,
-            html: `<strong>${req.body.email}</strong>`,
-            text: req.body.email
+        
+            try {
+                const mailSent = await transport.sendMail(message);
+                console.log(mailSent);
+            } catch(error) {
+                console.log(error);
+            }
+        
+            res.json({ success: true });
+        } else {
+            res.status(401).json({ error: "incorrect email" });
         }
-    
-        try {
-            const mailSent = await transport.sendMail(message);
-            console.log(mailSent);
-        } catch(error) {
-            console.log(error);
-        }
-    
-        res.json({ success: true });
     } else {
         res.status(401).json({ error: "unsent data" });
     }
